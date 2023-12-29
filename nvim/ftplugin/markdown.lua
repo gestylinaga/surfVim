5c1b343edf17c54a5a1bca99198196c1c5c440cb
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2023-12-29 14:07:27 -0800

    Version 3.3.3

---
+++ nvim/ftplugin/markdown.lua
@@ -0,0 +1,14 @@
+-- Markdown Specific Settings
+
+-- Enable Spellcheck
+vim.opt.spelllang = "en_us"
+vim.opt.spell = true
+
+-- Disable spellcheck with command `:set nospell`
+
+-- Spellcheck Keymaps:
+-- ]s -- next misspelled word
+-- [s -- previous misspelled word
+-- z= -- spelling suggestions
+-- zg -- add word to local dictionary
+-- zw -- remove word from local dictionary
