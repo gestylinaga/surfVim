9ea0572538a67b8bc45bd9d8351c9a58731a71ff
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2024-11-13 17:19:03 -0800

    Version 3.5.9

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -14,8 +14,7 @@ return {
       ensure_installed = {
         'lua_ls',
         'pyright',
-        'tsserver', --deprecated?
-        --'ts_ls',
+        'ts_ls',
         'cssls',
         'html',
         'bashls',
@@ -63,14 +62,6 @@ return {
     -- Call setup on each LSP server
     require('mason-lspconfig').setup_handlers({
       function(server_name)
-
-        ----------- temp fix ---------------
-        -- rename tserver to ts_ls
-        if server_name == 'tsserver' then
-          server_name = 'ts_ls'
-        end
-        local capabilities = require('cmp_nvim_lsp').default_capabilities()
-        ------------------------------------
 
         require('lspconfig')[server_name].setup({
           on_attach = lsp_attach,
